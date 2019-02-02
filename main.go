9be4c8dc709294af6c8df2910654e7b1bf2f1cc7
Haoyu(Sandy) Li <lihaoyu2678@yahoo.com>
2019-02-01 17:00:06 -0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+        fmt.Println("Hello World")
+	      fmt.Println("The current time is",time.Now().Format("2006-01-02T15:04:05.999999-07:00"))
+}
